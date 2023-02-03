75abdc1d8df6809938f36eb34cccd89d90a6aa91
Esakkiappan Thevar <thevarbalan32@gmail.com>
2023-02-03 18:18:25 +0530

    Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <navBar></navBar>
+   
     <router-view></router-view>
 
   </div>
@@ -7,10 +9,13 @@
 
 <script>
 
+import navBar from '@/components/Navbar.vue'
+
 
 export default {
   name: 'App',
   components: {
+    navBar
 
   }
 }
@@ -23,6 +28,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
--- src/init.js
+++ src/init.js
@@ -1,1 +1,7 @@
 import '@/services/plugins'
+import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
